c6e277f72e7eac1c88cbe86215dcd4c4d3473b70
Angel Newton <xanewton@gmail.com>
2017-11-03 14:58:53 -0400

    Fix game logic

--- Roshambo/ResultViewController.swift
+++ Roshambo/ResultViewController.swift
@@ -21,9 +21,11 @@ class ResultViewController: UIViewController {
     
     
     override func viewWillAppear(_ animated: Bool) {
+        let computerValue = randomValue()
+        
         // select the image according to the selection
         if let selectionValue = self.selectionValue {
-            assignImage(value: selectionValue)
+            calculateResults(player: selectionValue, machine: computerValue)
         } else {
             self.resultImage.image = nil;
         }
@@ -36,36 +38,64 @@ class ResultViewController: UIViewController {
         }
     }
     
-    // Assigns the image
-    func assignImage(value: Int) {
+    /**
+     * Randomly generates a Int from 1 to 3
+     */
+    func randomValue() -> Int {
+        // Generate a random Int32 using arc4Random
+        let randomValue = 1 + arc4random() % 3
         
-        switch value {
-        case 1: //Rock
-            self.resultImage.image = UIImage(named: "PaperCoversRock")
-        case 2: // Scissors
-            self.resultImage.image = UIImage(named: "RockCrushesScissors")
-        case 3: // Paper
-            self.resultImage.image = UIImage(named: "ScissorsCutPaper")
-        default:
-            self.resultImage.image = UIImage(named: "PaperCoversRock")
-        }
+        // Return a more convenient Int, initialized with the random value
+        return Int(randomValue)
     }
     
-    // Asssigne the message
-    func assignMessage(player: Int, machine: Int) {
-        switch player {
-        case 1:
+    
+    // Asssigne the messages and image
+    func calculateResults(player: Int, machine: Int) {
+        switch (player, machine) {
+        case (1, 1): // Rock, Rock
+            self.labelTitle.text = "It's a tie!"
+            self.labelMessage.text = "Rock vs Rock!"
+            self.resultImage.image = UIImage(named: "itsATie")
+        case (1, 2): // Rock, Scissors
             self.labelTitle.text = "You Win!"
-            self.labelMessage.text = "Paper covers rock!"
-        case 2:
+            self.labelMessage.text = "Rock breaks Scissors!"
+            self.resultImage.image = UIImage(named: "RockCrushesScissors")
+        case (1, 3): // Rock, Paper
+            self.labelTitle.text = "You Lose!"
+            self.labelMessage.text = "Rock are covered with Paper!"
+            self.resultImage.image = UIImage(named: "PaperCoversRock")
+            
+        case (2, 1): // Scissors, Rock
+            self.labelTitle.text = "You Lose!"
+            self.labelMessage.text = "Scissors are breaked with Rock!"
+            self.resultImage.image = UIImage(named: "RockCrushesScissors")
+        case (2, 2): // Scissors, Scissors
+            self.labelTitle.text = "It's a tie!"
+            self.labelMessage.text = "Scissors vs Scissors!"
+            self.resultImage.image = UIImage(named: "itsATie")
+        case (2, 3): // Scissors, Paper
             self.labelTitle.text = "You Win!"
-            self.labelMessage.text = "Paper covers rock!"
-        case 3:
+            self.labelMessage.text = "Scissors cut Paper!"
+            self.resultImage.image = UIImage(named: "ScissorsCutPaper")
+            
+        case (3, 1): // Paper, Rock
             self.labelTitle.text = "You Win!"
-            self.labelMessage.text = "Paper covers rock!"
+            self.labelMessage.text = "Paper covers Rock!"
+            self.resultImage.image = UIImage(named: "PaperCoversRock")
+        case (3, 2): // Paper, Scissors
+            self.labelTitle.text = "You Lose!"
+            self.labelMessage.text = "Paper is cut by Scissors!"
+            self.resultImage.image = UIImage(named: "ScissorsCutPaper")
+        case (3, 3): // Paper, Paper
+            self.labelTitle.text = "It's a tie!"
+            self.labelMessage.text = "Paper vs Paper!"
+            self.resultImage.image = UIImage(named: "itsATie")
+            
         default:
             self.labelTitle.text = "You Win!"
             self.labelMessage.text = "Paper covers rock!"
+            self.resultImage.image = UIImage(named: "PaperCoversRock")
         }
     }
     
--- Roshambo/SelectionViewController.swift
+++ Roshambo/SelectionViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class SelectionViewController: UIViewController {
     
-    /**
-     * Randomly generates a Int from 1 to 6
-     */
-    func randomValue() -> Int {
-        // Generate a random Int32 using arc4Random
-        let randomValue = 1 + arc4random() % 3
-        
-        // Return a more convenient Int, initialized with the random value
-        return Int(randomValue)
-    }
+    var user : Int = 0
+    
     
     // Prepare for launch a segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let controller = segue.destination as! ResultViewController
-        controller.selectionValue = randomValue()
+        
+        if self.user == 1 { // rock
+            controller.selectionValue = 1
+            self.user = 0
+        } else { // paper
+            controller.selectionValue = 3
+            self.user = 0
+        }
     }
     
     // Call the ResultsViewController
@@ -34,14 +33,16 @@ class SelectionViewController: UIViewController {
         let controller: ResultViewController
         controller = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
         
-        // Set the two values to random numbers from 1 to 6
-        controller.selectionValue = randomValue()
+        // Set the user value to scissors
+        controller.selectionValue = 2
         
         // Present the view Controller
         present(controller, animated: true, completion: nil)
     }
     
     @IBAction func callResultsUsingCodeAndStoryBoard() {
+        // user selected rock, mark it
+        self.user = 1
         performSegue(withIdentifier: "callResultsUsingCodeAndStoryBoard", sender: self)
     }
 
